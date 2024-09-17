c6260db5b286aa8dba6ecac3420b0350ca351f02
Deeksha D Achar <deekshadachar2001@gmail.com>
2024-09-17 12:14:19 +0530

    updated pallindrome solution

--- JavaPracticeHub/pallindrome.java
+++ JavaPracticeHub/pallindrome.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class pallindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+
+
+        if(Ispallindrome(num))
+            System.out.println("yes, It is a Pallindrome.");
+        else
+            System.out.println("No, It is not a pallindrome");
+    }
+
+    public static boolean Ispallindrome(int num) {
+        int originalnum = num;
+        int reversednum = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num = num / 10;
+            reversednum = reversednum * 10 + rem;
+
+        }
+        return originalnum == reversednum;
+    }
 }
